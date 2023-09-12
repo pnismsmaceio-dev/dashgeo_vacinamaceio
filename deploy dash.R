c3a155631a941b4a956796120ef386a8d662bc56
pnismsmaceio-dev <144747172+pnismsmaceio-dev@users.noreply.github.com>
2023-09-11 21:12:58 -0300

    Add files via upload

---
+++ deploy dash.R
@@ -0,0 +1,8 @@
+rsconnect::setAccountInfo(name='wkxw6z-m0rio0c0sar-j0nior',
+                          token='E7FAEFD7A669575EFBAE33AF24E89EAD', 
+                          secret='0q31f2CbkUa1K4/2bHHn7MKQy7eMTG0Rfhk6R/GD')
+
+rsconnect::deployApp("C:/Users/SONY/Documents/r/dash geo.rmd")
+
+
+deployApp("C:/Users/SONY/Documents/r/dash geo.rmd")
